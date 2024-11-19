40a1ed9cc8f37370d252382844f8bd20507d65f7
gabrielciprianoo <devgabrielcipriano@gmail.com>
2024-11-18 19:08:33 -0600

    Styles: Change css styles off interface

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,19 +28,20 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Recordar password') }}</span>
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
+                <x-link :href="route('password.request')">¿Olvidaste tu contraseña?</x-link>
+                <x-link :href="route('register')">¿No tienes una cuenta? Crea una</x-link>
             @endif
 
+        </div>
+        <div class="flex justify-end mt-4">
             <x-primary-button class="ms-3">
-                {{ __('Log in') }}
+                {{ __('Iniciar Sesion') }}
             </x-primary-button>
         </div>
     </form>
