8203589af80e3b23135cae45a7b4bafa3f21a37a
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-02-17 19:26:07 -0600

    style: inputs and guest view styles updated

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,7 +1,7 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
+    <ul {{ $attributes->merge(['class' => 'text-sm pl-2 bg-red-100 text-red-600 space-y-1 p-1 border-l-4 border-red-600']) }}>
         @foreach ((array) $messages as $message)
             <li>{{ $message }}</li>
         @endforeach
