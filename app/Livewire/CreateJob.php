db04f43b5a70c6368c23c6c55da5d1038ed4d50f
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-02-18 20:15:34 -0600

    feature: component to show create jobs form

---
+++ app/Livewire/CreateJob.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CreateJob extends Component
+{
+    public function render()
+    {
+        return view('livewire.create-job');
+    }
+}
---
+++ resources/views/livewire/create-job.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Form to public jobs here!</h1>
+</div>
